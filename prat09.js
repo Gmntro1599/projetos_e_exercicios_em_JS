10cd3df58e0152859614c308837d9fd96ec6fdab
Gabriel Monteiro <gabrielmonteiro1599@gmail.com>
2022-02-19 20:10:47 -0300

    Praticando condições

---
+++ prat09.js
@@ -0,0 +1,5 @@
+var media_anual = 30
+console.log(`a sua média anual de ${media_anual}`)
+if(media_anual < 28){ console.log('Pessimas noticias, você está reprovado. Tente de novo ano que vem!!')
+}
+if(media_anual > 28){ console.log('Parabéns, você está aprovdo!!')}
---
+++ prat10.js
@@ -0,0 +1,8 @@
+var gasto_caloria_dia = 500
+console.log(`Você tem o gasto calorico por dia de ${gasto_caloria_dia}`)
+if(gasto_caloria_dia >= 1000){
+    console.log('Você tem um otimo gasto calorico')
+}
+else{ 
+    console.log('Você tem um baixo gasto calorico!')
+}
